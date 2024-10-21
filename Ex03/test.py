484f2c71e7c2f63d8e57b0e038bd63cc0dc463a0
황주영 <dud9902@gmail.com>
2024-10-21 12:46:14 +0900

    2024.10.21

---
+++ Ex03/test.py
@@ -0,0 +1,39 @@
+# 0.import
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    # 1.연결/컨넥션 얻기
+    conn = mysql.connector.connect(
+        host="localhost",  # 호스트
+        user="phonebook",  # 사용자명
+        password="phonebook",  # 비밀번호
+        database="phonebook_db",  # 데이터베이스명
+    )
+
+    # 2.커서생성
+    cursor = conn.cursor()
+
+    # 3.SQL문준비  / 바인딩 / 실행
+    # --SQL문
+    query = """
+        insert into person
+        values(null, %s, %s, %s)
+    """
+    # --바인딩(튜플)
+    data = ("차은우", "010-7777-7777", "02-7777-7777")
+
+    # --실행
+    cursor.execute(query, data)  # 실행(임시)
+    conn.commit()  # 반영(실제)  리턴값은 없다
+
+    # 4.결과처리
+    print(f"{cursor.rowcount}개 저장")
+
+except Error as e:
+    print(f"데이터베이스 오류: {e}")
+
+finally:
+    # 5.자원정리
+    conn.close()
+    cursor.close()
